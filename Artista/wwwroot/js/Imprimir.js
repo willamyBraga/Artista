8aa752a7a5b52c56cdfc5d34d47974557ec92bd1
José Willamy Cunha Braga <willamybraga@yahoo.com>
2021-05-18 15:51:21 -0300

    pagina Detalhes

---
+++ Artista/wwwroot/js/Imprimir.js
@@ -0,0 +1,7 @@
+﻿function cont(){
+   var conteudo = document.getElementById('print').innerHTML;
+   tela_impressao = window.open('about:blank');
+   tela_impressao.document.write(conteudo);
+   tela_impressao.window.print();
+   tela_impressao.window.close();
+}
