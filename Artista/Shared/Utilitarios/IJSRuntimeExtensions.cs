c5a986a0fee6f236e26e9c1fc37fcb8ea0819892
willamybraga <willamybraga@yahoo.com>
2021-07-17 09:11:02 -0300

    ajustes

---
+++ Artista/Shared/Utilitarios/IJSRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artista.Shared.Utilitarios
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string fileName, byte[] content)
+        {
+            return js.InvokeAsync<object>("saveAsFile", fileName, Convert.ToBase64String(content));
+        }
+    }
+}
