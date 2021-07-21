2323eb39f6d73f2d92eed0a4846d636411858e39
willamybraga <willamybraga@yahoo.com>
2021-07-21 08:27:05 -0300

    ajustes

--- Artista/Startup.cs
+++ Artista/Startup.cs
@@ -38,6 +38,8 @@ namespace Artista
 
             services.AddServerSideBlazor();
             services.AddSingleton(conexao);
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +60,8 @@ namespace Artista
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseRequestLocalization("pt-BR");
 
             app.UseEndpoints(endpoints =>
             {
